b7657882e1e529d8e784440bc0b389ee5e36af0e
Jaime Arrocha <jarr@innercoder.com>
2016-01-13 20:06:48 -0600

    -m

---
+++ common.h
@@ -0,0 +1,11 @@
+/*
+ * Implementation of common functions
+ */
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+void error_msg(char *);
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+ * cweb_server: c web server implementation
+ */
+
+#include <stdio.h>
+#include <stdlig.h>
+#include <sys/socket.h>
+
+int main(int argc, char **argv)
+{
+
+
+
+}
